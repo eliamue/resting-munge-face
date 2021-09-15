f48cb9d612de15c3738af25fca65d07430221cb1
Elise Muellerleile <eliamue@live.com>
2021-09-15 18:40:56 -0500

    Display framework added

--- src/components/resty/Controls.test.jsx
+++ src/components/resty/Controls.test.jsx
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Controls from './Controls';
---
+++ src/components/resty/Display.test.jsx
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Display from './Display';
+
+
